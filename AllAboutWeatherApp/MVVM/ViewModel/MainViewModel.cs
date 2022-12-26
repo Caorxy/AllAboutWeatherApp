abdfa390688302202744fd2ac917e80965c4ced5
Caorxy_ <gabriel.pawlak@op.pl>
2022-12-26 21:57:38 +0100

    Home layout completed

---
+++ AllAboutWeatherApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AllAboutWeatherApp.Core;
+
+namespace AllAboutWeatherApp.MVVM.ViewModel;
+
+public class MainViewModel : ObservableObject
+{
+    public HomeViewModel HomeVm { get; set; }
+    private object? _currentView;
+
+    public object? CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    
+    
+    public MainViewModel()
+    {
+        HomeVm = new HomeViewModel();
+        CurrentView = HomeVm;
+    }
+}
